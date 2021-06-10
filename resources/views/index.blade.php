285973bd0f988a5815fc6d8b0f7ea8a46ec9ce1f
Amakye Oduro Philip <philamakye@gmail.com>
2021-06-10 17:28:11 +0100

    Homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,18 @@
 <style>
   .carousel img{
 height: 35em;
-
   }
+
+.nopadding{
+  padding: 0 !important;
+   
+}
+
+.nomargin{
+  margin: 0 !important;
+}
+
+  
 </style>
 @extends('layouts.app')
 
@@ -36,94 +46,263 @@ height: 35em;
       <hr>  
     </h2>
     
-    <div class="row text-center trio">
+    <div class="row text trio">
 
       <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card h-100">
+          <div class="card h-150">
               <img class="card-img-top" src="images/cards/card1.jpg" alt="Img" style="height:40%">
-              <div class="card-body">
-                <div class="container mt-0">
-                  <div class="row my-0">
-                    <div class="col mt-0">
-                      <p class="lead ml-0">
+              <div class="card-body nopadding mt-2" style="margin-left:0px;">
+                <div class="container nopadding">
+                  <div class="row ">
+                    <div class="col ">
+                      <p class="lead pl-2 nomargin " style="color: green" >
                         Funding
                       </p>
                     </div>
-                    <div class="col">
-                      <p class="lead mr-0">
-                        <i class="far fa-heart"></i>
+                    <div class="col nopadding">
+                      <p class="lead nomargin pr-4" style="float: right">
+                        <i class="far fa-heart "></i>
                       </p>
                     </div>
-                  
                   </div>  
-                   <hr>
+                  <hr>  
                 </div>
              
-                  <h4 class="card-title">Dwarf Telescope - A portable digital telescope</h4>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  <h4 class="card-title px-2">Dwarf Telescope - A portable digital telescope</h4>
+                  <p class="card-text px-2">
+                    <small style="font-size:1em">See your friends clearly from 2000 meters away</small>
+                  </p>
+                  <p class="card-text px-2 nomargin">
+                    <small class="text-muted" style="font-size: 1.1em">TECH AND INNOVATION</small>
+                  </p>
+                 
+                  <div class="container nopadding">
+                    <div class="row ">
+                      <div class="col ">
+                        <p class="lead pl-2 nomargin " style="font-size: 1em " >
+                          24,546 GHS raised 
+                        </p>
+                      </div>
+                      <div class="col nopadding">
+                        <p class="lead nomargin pr-4" style="float: right; font-size:1em">
+                          76%
+                        </p>
+                      </div>
+                    </div>  
+                    <div class="row mx-2" >
+                      <div class="progress nopadding" style="height: 10px;">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 76%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                    <div class="row px-3 pt-2 pb-0">
+                     
+                      <p>
+                        <i class="far fa-clock"></i><small class="text-muted pl-2" >33 days left</small>
+                      </p>
+                      
+
+                    </div>
+                     
+                  </div>
+
               </div>
               <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Find Out More!</a>
+                  <a href="#" class="btn btn" style="background-color: rgb(50, 18, 156); color: white">Find Out More!</a>
               </div>
           </div>
       </div>
 
       <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card h-100" ">
-              <img class="card-img-top" src="images/cards/card2.jpg" alt="Img" style="height:40%">
-              <div class="card-body">
-
-                <div class="container mt-0">
-                  <div class="row my-0">
-                    <div class="col mt-0">
-                      <p class="lead ml-0">
-                        Funding
+        <div class="card h-150">
+            <img class="card-img-top" src="images/cards/card2.jpg" alt="Img" style="height:40%">
+            <div class="card-body nopadding mt-2" style="margin-left:0px;">
+              <div class="container nopadding">
+                <div class="row ">
+                  <div class="col ">
+                    <p class="lead pl-2 nomargin " style="color: green" >
+                      Funding
+                    </p>
+                  </div>
+                  <div class="col nopadding">
+                    <p class="lead nomargin pr-4" style="float: right">
+                      <i class="far fa-heart "></i>
+                    </p>
+                  </div>
+                </div>  
+                <hr>  
+              </div>
+           
+                <h4 class="card-title px-2">Dwarf Telescope - A portable digital telescope</h4>
+                <p class="card-text px-2">
+                  <small style="font-size:1em">See your friends clearly from 2000 meters away</small>
+                </p>
+                <p class="card-text px-2 nomargin">
+                  <small class="text-muted" style="font-size: 1.1em">TECH AND INNOVATION</small>
+                </p>
+               
+                <div class="container nopadding">
+                  <div class="row ">
+                    <div class="col ">
+                      <p class="lead pl-2 nomargin " style="font-size: 1em " >
+                        24,546 GHS raised 
                       </p>
                     </div>
-                    <div class="col">
-                      <p class="lead mr-0">
-                        <i class="far fa-heart"></i>
+                    <div class="col nopadding">
+                      <p class="lead nomargin pr-4" style="float: right; font-size:1em">
+                        76%
                       </p>
                     </div>
-                  
                   </div>  
-                   <hr>
+                  <div class="row mx-2" >
+                    <div class="progress nopadding" style="height: 10px;">
+                      <div class="progress-bar bg-success" role="progressbar" style="width: 76%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="row px-3 pt-2 pb-0">
+                   
+                    <p>
+                      <i class="far fa-clock"></i><small class="text-muted pl-2" >33 days left</small>
+                    </p>
+                    
+
+                  </div>
+                   
                 </div>
 
-                  <h4 class="card-title">Card title</h4>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-              </div>
-              <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Find Out More!</a>
-              </div>
-          </div>
-      </div>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn" style="background-color: rgb(50, 18, 156); color: white">Find Out More!</a>
+            </div>
+        </div>
+    </div>
 
-      <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card h-100">
-              <img class="card-img-top" src="images/cards/card3.jpg" alt="Img" style="height: 40%">
-              <div class="card-body">
-                  <h4 class="card-title">Card title</h4>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-              </div>
-              <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Find Out More!</a>
-              </div>
-          </div>
-      </div>
+    <div class="col-lg-3 col-md-6 mb-4">
+      <div class="card h-150">
+          <img class="card-img-top" src="images/cards/card3.jpg" alt="Img" style="height:40%">
+          <div class="card-body nopadding mt-2" style="margin-left:0px;">
+            <div class="container nopadding">
+              <div class="row ">
+                <div class="col ">
+                  <p class="lead pl-2 nomargin " style="color: green" >
+                    Funding
+                  </p>
+                </div>
+                <div class="col nopadding">
+                  <p class="lead nomargin pr-4" style="float: right">
+                    <i class="far fa-heart "></i>
+                  </p>
+                </div>
+              </div>  
+              <hr>  
+            </div>
+         
+              <h4 class="card-title px-2">Dwarf Telescope - A portable digital telescope</h4>
+              <p class="card-text px-2">
+                <small style="font-size:1em">See your friends clearly from 2000 meters away</small>
+              </p>
+              <p class="card-text px-2 nomargin">
+                <small class="text-muted" style="font-size: 1.1em">TECH AND INNOVATION</small>
+              </p>
+             
+              <div class="container nopadding">
+                <div class="row ">
+                  <div class="col ">
+                    <p class="lead pl-2 nomargin " style="font-size: 1em " >
+                      24,546 GHS raised 
+                    </p>
+                  </div>
+                  <div class="col nopadding">
+                    <p class="lead nomargin pr-4" style="float: right; font-size:1em">
+                      76%
+                    </p>
+                  </div>
+                </div>  
+                <div class="row mx-2" >
+                  <div class="progress nopadding" style="height: 10px;">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 76%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+                <div class="row px-3 pt-2 pb-0">
+                 
+                  <p>
+                    <i class="far fa-clock"></i><small class="text-muted pl-2" >33 days left</small>
+                  </p>
+                  
 
-      <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card h-100">
-              <img class="card-img-top" src="images/cards/card4.jpeg" alt="Img" style="height:40%">
-              <div class="card-body">
-                  <h4 class="card-title">Card title</h4>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+                </div>
+                 
               </div>
-              <div class="card-footer">
-                  <a href="#" class="btn btn-primary">Find Out More!</a>
-              </div>
+
+          </div>
+          <div class="card-footer">
+              <a href="#" class="btn btn" style="background-color: rgb(50, 18, 156); color: white">Find Out More!</a>
           </div>
       </div>
+  </div>
+
+  <div class="col-lg-3 col-md-6 mb-4">
+    <div class="card h-150">
+        <img class="card-img-top" src="images/cards/card4.jpeg" alt="Img" style="height:40%">
+        <div class="card-body nopadding mt-2" style="margin-left:0px;">
+          <div class="container nopadding">
+            <div class="row ">
+              <div class="col ">
+                <p class="lead pl-2 nomargin " style="color: green" >
+                  Funding
+                </p>
+              </div>
+              <div class="col nopadding">
+                <p class="lead nomargin pr-4" style="float: right">
+                  <i class="far fa-heart "></i>
+                </p>
+              </div>
+            </div>  
+            <hr>  
+          </div>
+       
+            <h4 class="card-title px-2">Dwarf Telescope - A portable digital telescope</h4>
+            <p class="card-text px-2">
+              <small style="font-size:1em">See your friends clearly from 2000 meters away</small>
+            </p>
+            <p class="card-text px-2 nomargin">
+              <small class="text-muted" style="font-size: 1.1em">TECH AND INNOVATION</small>
+            </p>
+           
+            <div class="container nopadding">
+              <div class="row ">
+                <div class="col ">
+                  <p class="lead pl-2 nomargin " style="font-size: 1em " >
+                    24,546 GHS raised 
+                  </p>
+                </div>
+                <div class="col nopadding">
+                  <p class="lead nomargin pr-4" style="float: right; font-size:1em">
+                    76%
+                  </p>
+                </div>
+              </div>  
+              <div class="row mx-2" >
+                <div class="progress nopadding" style="height: 10px;">
+                  <div class="progress-bar bg-success" role="progressbar" style="width: 76%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <div class="row px-3 pt-2 pb-0">
+               
+                <p>
+                  <i class="far fa-clock"></i><small class="text-muted pl-2" >33 days left</small>
+                </p>
+                
+
+              </div>
+               
+            </div>
+
+        </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn" style="background-color: rgb(50, 18, 156); color: white">Find Out More!</a>
+        </div>
+    </div>
+</div>
 
   </div>
  
