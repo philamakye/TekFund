927b26bcedccdc64106605494b4724e50046ae7a
Amakye Oduro Philip <philamakye@gmail.com>
2021-06-11 15:52:05 +0100

    Login touch up done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,150 @@
+<style>
+    .container .icon {
+    margin-top: 0px;
+    padding-top: 10px;
+}
+
+.container .icon img {
+    height: 90px;
+}
+
+.sign button:hover{
+    background-color: rgb(50, 18, 156);
+    color: white;
+}
+.sign-box {
+    margin-top: 10px;
+    padding: 30px;
+    border-style: solid;
+    border-width: 1.9px;
+    border-radius: 2%;
+    border-color: rgb(212, 216, 221);
+}
+
+.container .show-button:hover {
+    cursor: pointer;
+    background-color: rgb(50, 18, 156);
+}
+</style>
+
 @extends('layouts.app')
 
 @section('content')
 
         <center><h2><img src="images/icon.gif" class="img-fluid mr-2" style="height: 2.0em; border-radius:30px" alt="...">TekFund</h2></center>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-3">
+                
+              </div>
+              <div class="col-sm icon">
+             
+             <div class=" sign-box">
+          
+              <center><h3>Sign-In</h3></center>
+          
+          
+               <form method="POST" action="{{ route('login') }}">
+                                  @csrf
+          
+                                  <div class="form-group row">
+                                      <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+          
+                                      <div class="col-md-6">
+                                          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+          
+                                          @error('email')
+                                              <span class="invalid-feedback" role="alert">
+                                                  <strong>{{ $message }}</strong>
+                                              </span>
+                                          @enderror
+                                      </div>
+                                  </div>
+          
+          
+          
+                                  <div class="form-group row">
+                                      <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+          
+                                      <div class="col-md-6">
+                                          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+          
+                                          @error('password')
+                                              <span class="invalid-feedback" role="alert">
+                                                  <strong>{{ $message }}</strong>
+                                              </span>
+                                          @enderror
+                                      </div>
+                                  </div>
+          
+          
+          
+                                  <div class="form-group row">
+                                      <div class="col-md-6 offset-md-4">
+                                          <div class="form-check">
+                                              <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+          
+                                              <label class="form-check-label" for="remember">
+                                                  {{ __('Remember Me') }}
+                                              </label>
+                                          </div>
+                                      </div>
+                                  </div>
+          
+          
+          
+                                  <div class="form-group row mb-0">
+                                      <div class="col-md-8 offset-md-4">
+                                          <button type="submit" class="btn btn" style="background-color: rgb(50, 18, 156); color:white">
+                                              {{ __('Login') }}
+                                          </button>
+          
+                                          @if (Route::has('password.request'))
+                                              <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                  {{ __('Forgot Your Password?') }}
+                                              </a>
+                                          @endif
+                                      </div>
+                                  </div>
+          
+                              </form>
+          
+             <center>
+             <p><small> By continuing, you agree to Tekfund's  <a href="#" style="text-decoration: none;">Conditions of Use</a> and <a href="#" style="text-decoration: none;">Privacy Notice</a>.</small></p>
+              <p><small><i class="fas fa-caret-right"></i> <a href="#" style="text-decoration: none;">Need help?</a></small></p>
+             </center>
             </div>
-        </div>
-    </div>
-</div>
+            <div class="mt-3" style="width: 100%; height: 20px; border-bottom: 1px solid rgb(212, 216, 221); text-align: center">
+            <span style="font-size:small; color:rgb(135, 136, 138); background-color: white; padding: 0 5px;">
+              New to TekFund?
+            </span>
+          </div> 
+          <form action="{{ route('register') }}""> 
+          <div class="d-grid gap-2 mt-3 sign">     
+          <button type="submit" style="border-radius:3px; border-width: 1.5px;"> Create your TekFund account</button>
+        </div>  
+         </form>  
+          </div>
+              <div class="col-sm-3">
+                
+              </div>   
+            </div>
+          <div class="row mt-4" >
+          <div class="col-sm"></div>
+          <div class="col-sm">
+          
+          
+          <div>
+          </div>
+          </div>
+          <div class="col-sm"></div>
+          <div>
+          <hr>
+          <center><p><small><a href="#" style="text-decoration: none; padding-right:5%; font: size 0.5em;">Condition of Use</a>  <a href="#" style="text-decoration: none; padding-right:5%; font: size 0.5em;">Privacy Notice</a>  <a href="#" style="text-decoration: none; font: size 0.5em;"> Help</a></small></p></center>
+          <center><p><small>© 2021, TekFund.com, Inc. or its affiliates</small></p></center>
+          </div>
+          </div>
+          
+          </div>
 @endsection
