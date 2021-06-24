00c4083de3ff86fde55aefdcf472f583b7ed7cff
Philip Amakye <Philip@philamakye@gmail.com>
2021-06-24 14:06:15 -0700

    Register corrected

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:user xs'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'username' => ['required','string','min:4','unique:users'],
             'phone_number' =>['required','min:10','max:10'],
