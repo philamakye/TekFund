930699a4e9c94e309ccd2c584d2e0df7cb65413c
heywest <heyitzwest@gmail.com>
2021-06-25 17:16:49 +0000

    new change

--- routes/web.php
+++ routes/web.php
@@ -29,21 +29,16 @@ Route::get('/', function () {
 Route::get('/campaign', function () {
     return view('viewcampaign');
 });
-Route::get('/multi', function () {
-    return view('healthcare.multistep');
-});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/startcampaign', [App\Http\Controllers\CampaignController::class, 'index'])->name('start campaign');
 
-//Route::view('admin','admin');
 
 Route::get('/admin', 'AdminController@index' )->name('admin')->middleware('admin');
 Route::get('/setting',[App\Http\Controllers\UserController::class, 'index'])->name('settings');
 Route::get('/healthcare', 'CampaignController@health_step1')->name('healthcare');
-//Route::get('/healthcare2', 'CampaignController@health_step2')->name('healthcare-step 2');
 Route::get('/editprofile', 'UserController@profile2')->name('profile-2');
 Route::get('/email', 'UserController@email')->name('Email');
 Route::get('/profile', 'UserController@profile')->name(('Profile'));
@@ -52,11 +47,8 @@ Route::post('/upload-avatar', 'userController@uploadImg')->name('Upload-avatar')
 Route::get('/contributions','UserContributionController@index')->name('Contributions');
 Route::get('/mycampaigns','CampaignController@mycamps')->name('My Campaigns');
 Route::get('/others','CampaignController@others_step1')->name('Others');
-Route::get('/others2','CampaignController@others_step2')->name('Others-step 2');
 Route::get('/project','CampaignController@proj_step1')->name('project');
-Route::get('/project2','CampaignController@proj_step2')->name('project-step 2');
 Route::get('/startup', 'CampaignController@startup_step1')->name('Start Up');
-Route::get('/startup2','CampaignControler@startup_step2')->name('Start Up-step 2');
 
 
 
