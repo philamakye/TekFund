db740aef387eb4c5492fd9f3d929b866d2fa9a08
Philip Amakye <Philip@philamakye@gmail.com>
2021-07-01 08:05:24 +0100

    View campaign

--- resources/views/viewcampaign.blade.php
+++ resources/views/viewcampaign.blade.php
@@ -1,39 +1,60 @@
+<style>
+    .nomargin{
+        margin: 0 !important;
+    }
+
+    .nopadding{
+        padding:0 !important;
+    }
+
+    html, body. div{
+    margin: 0 !important;
+    padding: 0 !important;
+    width: 100%;
+}
+</style>
 @extends('layouts.app')
 @section('content')
-<div class="card" style="margin-left:70px; width: 90%;  height: 500px;  backgrond-color: whitesmoke;">
-    <div class="card-body">
 
-        <iframe width="660" height="350" style="float: left" src="https://www.youtube.com/embed/BXiHvgrJfkg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div class="row text-left">
-        <h3 style="margin-left: 150px; color:rgb(48, 168, 156)"><b>Funding</b></h3>
-        <h2 style="margin-left: 150px; margin-top:30px;"><b>Smart Crops Building Machine</b></h2>
-        <h3 style="margin-left: 150px;">Quicker and Satiating</h3>
-        <h5 style="margin-left: 150px; padding-right:200px; margin-top:10px;">The first ever smart crop making machine to ever exist in the planet. Created by Jehu Stevenson</h5>
-        <div class="row">
-            <div class="col-md-10">
-        <!--<img src="https://images.fairtrade.net/_articleFull/FLO30561.jpg" alt="avatar" style="margin-left: 150px; height: 70px; width: 150px; margin-top: 15px;"/>-->
-
-        <h4 style="margin-left:150px; margin-top:10px">Smart Crop Machine</h4>
-        <h5 style="margin-left:150px; margin-top:11px;">1 campaign</h5> <div class="vl"style="border-left: 3px solid #6b6565; height: 18px; position: absolute; left: 50%; margin-left: -3px; top: 48px;"><h5 style="margin-left:7px;"> Kumasi, Ghana</h5>
-        </div>
-            </div>
-        </div>
-        <div class="progress" style="margin-left:150px; width:70%; padding:0;">
-  <div class="progress-bar w-70" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-</div>
+<div class="container-fluid" >
+    <div class="row px-2 mx-1" style="border: 1px solid rgb(155, 146, 155); ">
+      <div class="col px-3 py-3">
+          <div class="ratio ratio-16x9">
+        <iframe  src="https://www.youtube.com/embed/BXiHvgrJfkg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
     </div>
-</div>
+      <div class="col  px-3 py-3" >
+  <h3 style="color: turquoise"><b>Funding</b></h3>
+  <h3><b>SMART CROPS BUILDING MACHINE</b></h3>
+  <h3>Quicker and Satiating</h3>
+  <h4> The first ever smart crop making machine to ever exist on this planet. Created by Jehu Stevenson</h4>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+  <div class="container py-1">
+    <div class="row">
+      <div class="col-2">
+        <img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid" alt="...">
+      </div>
+      <div class="col">
+       <h4><b>SMART CROP MACHINE<b></h4>
+       <h4> 1 Campaign | Kumasi, Ghana</h4>
+      </div>
+    </div>
+  </div>
+
+  <a>GHC 24,000</a>
+  <a style="float: right">2,500 backers</a>
+  <div class="progress">
+    <div class="progress-bar bg-info" role="progressbar" style="width: 90%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+  <a>90% of GHC 26,000</a>
+  <a style="float: right">31 days left</a>
+
+    </div>
+
+    </div>
+  </div>
+
+
 <br>
 <br>
 <br>
