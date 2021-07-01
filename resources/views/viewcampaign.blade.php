3cdb5778130fa95487ac67f1f5cb7b61c1839e81
Philip Amakye <Philip@philamakye@gmail.com>
2021-07-01 10:54:54 +0100

    View Campaign more or less completed

--- resources/views/viewcampaign.blade.php
+++ resources/views/viewcampaign.blade.php
@@ -19,17 +19,28 @@
 <div class="container-fluid" >
     <div class="row px-2 mx-1" style="border: 1px solid rgb(155, 146, 155); ">
       <div class="col px-3 py-3">
-          <div class="ratio ratio-16x9">
+          <div class="mb-2 ratio ratio-16x9">
         <iframe  src="https://www.youtube.com/embed/BXiHvgrJfkg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
+
+          <center>
+              <a href=""><img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid pl-1" alt="..."></a>
+              <a href=""><img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid pl-1" alt="..."></a>
+              <a href=""><img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid pl-1" alt="..."></a>
+              <a href=""><img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid pl-1" alt="..."></a>
+              <a href=""><img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid pl-1" alt="..."></a>
+              <a href=""><img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid pl-1" alt="..."></a>
+          </center>
+
     </div>
-      <div class="col  px-3 py-3" >
+
+   <div class="col  px-3 py-3" >
   <h3 style="color: turquoise"><b>Funding</b></h3>
   <h3><b>SMART CROPS BUILDING MACHINE</b></h3>
   <h3>Quicker and Satiating</h3>
   <h4> The first ever smart crop making machine to ever exist on this planet. Created by Jehu Stevenson</h4>
 
-  <div class="container py-1">
+  <div class="container pt-3 pb-3">
     <div class="row">
       <div class="col-2">
         <img src="images/north_shore_sunset_beach6869.jpg" style="height:4em" class="img-fluid" alt="...">
@@ -49,15 +60,263 @@
   <a>90% of GHC 26,000</a>
   <a style="float: right">31 days left</a>
 
+
+  <div class="container mt-3">
+      <a href="" class="mr-5 px-4 py-2" style="background-color: rgb(65, 39, 151); color:white; text-decoration:none;">Back it</a>
+      <a href="" class="mr-5 px-3 py-2" style="background-color: whitesmoke; color:black; border:1px solid; text-decoration:none;" ><i class="far fa-heart"></i> Follow</a>
+      <a href="" class="mr-1 px-1 py-2 " style="color:rgb(51, 51, 248); font-size: 1.5em"><i class="fab fa-facebook-f"></i></a>
+      <a href="" class="mr-2 px-1 py-2" style="color:rgb(66, 201, 241); font-size: 1.5em"><i class="fab fa-twitter"></i></a>
+      <a href="" class="mr-2 px-1 py-2" style="color:rgb(116, 119, 121); font-size: 1.5em"><i class="fas fa-link"></i></a>
+  </div>
+
     </div>
 
     </div>
   </div>
 
+<div class="container mt-5">
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+          <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true"><b>Story</b></button>
+          <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false"><b>Updates</b></button>
+          <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false"><b>Comments</b></button>
+        </div>
+      </nav>
+      <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
 
+            <div class="container-fluid px-3" style="border: 1px solid">
+            <div class="pt-1">
+               <h3><b>Design Summary</b></h3>
+               <p>
+                   <h4>
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, natus incidunt! Quidem id sit quasi beatae repellendus error qui, adipisci mollitia non placeat quae facilis neque dicta sapiente optio ex?
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi voluptas et, ullam, a magni reiciendis tenetur ut pariatur laudantium repudiandae molestiae quisquam excepturi quos obcaecati fugit at nostrum. Alias, eum!
+                   </h4>
+                </p>
+            </div>
+           </div>
+
+           <figure class="figure pt-5">
+            <img src="images/crop1.svg" class="img-fluid" alt="...">
+            <figcaption class="figure-caption">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil asperiores porro tempore explicabo iure suscipit tenetur, id obcaecati voluptates officia nisi, amet voluptatum, qui ipsa autem quo dolor dolorem. Id!
+                lore
+            </figcaption>
+           </figure>
+
+           <div class="my-3">
+               <h3>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod repellat tempora fuga architecto placeat inventore temporibus consequuntur dignissimos! Libero perspiciatis corrupti facere ducimus nostrum consectetur impedit dolores asperiores enim sed! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sit autem dolor molestias? Dicta doloremque beatae iste voluptatem architecto accusamus nam amet corrupti corporis non possimus, consequatur quod voluptas laborum.
+               </h3>
+           </div>
+
+           <figure class="figure pt-5">
+            <img src="images/crop2.svg" class="img-fluid" alt="...">
+            <figcaption class="figure-caption">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil asperiores porro tempore explicabo iure suscipit tenetur, id obcaecati voluptates officia nisi, amet voluptatum, qui ipsa autem quo dolor dolorem. Id!
+                lore
+            </figcaption>
+           </figure>
+
+           <div class="my-3">
+               <h3>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod repellat tempora fuga architecto placeat inventore temporibus consequuntur dignissimos! Libero perspiciatis corrupti facere ducimus nostrum consectetur impedit dolores asperiores enim sed! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sit autem dolor molestias? Dicta doloremque beatae iste voluptatem architecto accusamus nam amet corrupti corporis non possimus, consequatur quod voluptas laborum.
+               </h3>
+           </div>
+
+        </div>
+        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+
+            <div class="container-fluid px-3" style="border: 1px solid">
+                <div class="pt-1">
+                   <h3><b>Production Update</b></h3>
+                   <p>
+                       <h4>
+                       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, natus incidunt! Quidem id sit quasi beatae repellendus error qui, adipisci mollitia non placeat quae facilis neque dicta sapiente optio ex?
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi voluptas et, ullam, a magni reiciendis tenetur ut pariatur laudantium repudiandae molestiae quisquam excepturi quos obcaecati fugit at nostrum. Alias, eum!
+                       </h4>
+                    </p>
+                </div>
+               </div>
+
+               <figure class="figure pt-5">
+                <img src="images/crop1.svg" class="img-fluid" alt="...">
+                <figcaption class="figure-caption">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil asperiores porro tempore explicabo iure suscipit tenetur, id obcaecati voluptates officia nisi, amet voluptatum, qui ipsa autem quo dolor dolorem. Id!
+                    lore
+                </figcaption>
+               </figure>
+
+               <div class="my-3">
+                   <h3>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod repellat tempora fuga architecto placeat inventore temporibus consequuntur dignissimos! Libero perspiciatis corrupti facere ducimus nostrum consectetur impedit dolores asperiores enim sed! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sit autem dolor molestias? Dicta doloremque beatae iste voluptatem architecto accusamus nam amet corrupti corporis non possimus, consequatur quod voluptas laborum.
+                   </h3>
+               </div>
+
+               <figure class="figure pt-5">
+                <img src="images/crop2.svg" class="img-fluid" alt="...">
+                <figcaption class="figure-caption">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil asperiores porro tempore explicabo iure suscipit tenetur, id obcaecati voluptates officia nisi, amet voluptatum, qui ipsa autem quo dolor dolorem. Id!
+                    lore
+                </figcaption>
+               </figure>
+
+               <div class="my-3">
+                   <h3>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod repellat tempora fuga architecto placeat inventore temporibus consequuntur dignissimos! Libero perspiciatis corrupti facere ducimus nostrum consectetur impedit dolores asperiores enim sed! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus sit autem dolor molestias? Dicta doloremque beatae iste voluptatem architecto accusamus nam amet corrupti corporis non possimus, consequatur quod voluptas laborum.
+                   </h3>
+               </div>
+
+
+        </div>
+        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+
+
+            <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="row my-2">
+                <div class="col-sm-2 py-1">
+                    <img src="images/Sebastiaan_ter_Burg_profile@2x.jpg" style="border-radius: 50%" class="img-fluid" alt="...">
+                </div>
+                <div class="col-sm">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title"><b>Kofi Sternberg</b>
+                      <a style="float: right">25 minutes ago</a>
+                     </h5>
+                      <p class="card-text">This is where backers of the campaign can comment and make certain suggestions on the campaign
+                          Users who are not backers are not eiligible to comment on campaigns  </p>
+                      <a style="font-size: 1.5em; color:rgb(52, 130, 233)"><i class="far fa-thumbs-up"></i> 87 </a>
+                      <a style="font-size:1.5em;"> | </a>
+                      <a style="font-size: 1.5em; color:rgb(250, 36, 36)" ><i class="far fa-thumbs-down"></i> 420</a>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+        </div>
+      </div>
+</div>
 <br>
-<br>
-<br>
-<br>
-<br>
+
 @endsection
