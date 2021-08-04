7d438dc8a5c351365a059d4a5b852ea1ea82aca7
heywest <79490695+heyWest@users.noreply.github.com>
2021-08-04 09:25:06 +0000

    push

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,15 +2,14 @@
 @section('content')
 <form action="profile" method="GET" style="display: inline" >
 <button class="btn btn-primary" style="margin-left: 15px;background-color: rgb(44, 20, 124); color: white;" type="submit"><i class="fa fa-eye" aria-hidden="false"></i>
-View Profile <span class="badge bg-secondary"></span>
+VIEW PROFILE <span class="badge bg-secondary"></span>
 </button>
 </form>
 <form action="editprofile" method="GET" style="display: inline" >
 <button class="btn btn-primary" style="background-color: white; color: black;" type="submit"><i class="fa fa-pencil" aria-hidden="true"></i>
-Edit Profile & Settings<span class="badge bg-secondary"></span>
+EDIT PROFILE & SETTINGS<span class="badge bg-secondary"></span>
 </button>
 </form>
-
 
 <h2 style="margin-left:30px; font-family: cooper; margin-top:10px; font-size: 25px;text-transform: uppercase;">{{ Auth::user()->name}}</h2>
 @if (Auth::user()->city)
