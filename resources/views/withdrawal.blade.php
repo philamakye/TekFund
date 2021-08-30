e5313f7284b54b1bd019758f63bdebf99483f74a
caprime <oforige@gmail.com>
2021-08-30 17:20:35 +0000

    UI

--- resources/views/withdrawal.blade.php
+++ resources/views/withdrawal.blade.php
@@ -1,4 +1,19 @@
 @extends('layouts.app')
+<style>
+    /* main{
+    padding-top: 0 !important;
+    margin-bottom: 0px !important;
+    color: black !important;
+    background: turquoise;
+} */
+</style>
 @section('content')
-    This is the withdrawal page.
+    
+    <div class="container-fluid" style="background: turquoise;">
+    <div class="row">
+        <div class="col col-lg-5">
+            
+        </div>
+    </div>
+    </div>
 @endsection
